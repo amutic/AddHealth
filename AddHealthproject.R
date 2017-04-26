54d99f1b88a19ecde55fbad689a485c7109a3dc9
Mutic <ahende8@emory.edu>
2017-04-26 08:08:13 -0400

    Final project updated

---
+++ AddHealthproject.R
@@ -0,0 +1,12 @@
+---
+  title: "Number of Sexual Partners, Depression, and Anxiety"
+author: "Abby Mutic"
+date: "March 29, 2017"
+output:
+  pdf_document: default
+html_document: default
+word_document: default
+---
+  
+  
+  
